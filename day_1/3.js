e96accab05f365faf834aed34197826b21d3fc79
Rajeev-55 <razevmakaju@gmail.com>
2026-01-06 22:44:51 +0545

    Delete day_1 directory

--- day_1/3.js
+++
@@ -1,8 +0,0 @@
-/*Task 3 – Arrow Function Simple
- Write an arrow function greet that takes a name and returns "Hello, <name>!". Test it with your name */
-
-// The arrow function
-const greet = (name) => `Hello, ${name}`;
-
-// Testing it with your name
-console.log(greet("Ram Nepal"));
