cf184ad3333a6f0cf0c99b2b77b23b0845e69970
Rajeev-55 <razevmakaju@gmail.com>
2026-01-06 21:06:59 +0545

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+/*Task 1 – Variables and Template Literals
+ Create two variables, firstName and lastName, and combine them into a greeting message using template literals. Log the message.
+*/
+const firstName = "Rajeev";
+const lastName = "Makaju";
+
+// Combine them using backticks
+const greeting = `Hello, ${firstName} ${lastName}!`;
+
+console.log(greeting); 
+// Result: Hello, Rajeev Makaju!
